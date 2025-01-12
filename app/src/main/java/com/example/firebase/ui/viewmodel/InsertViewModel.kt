05c9ebdc061489388661c0be0c11ada2cc2298d5
Anggia Intan <intananggia23@gmail.com>
2025-01-12 10:44:28 +0700

    membuat data class MahasiswaEvent

--- app/src/main/java/com/example/firebase/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/firebase/ui/viewmodel/InsertViewModel.kt
@@ -1,8 +1,20 @@
 package com.example.firebase.ui.viewmodel
 
+import androidx.lifecycle.Lifecycle
+import com.example.firebase.model.Mahasiswa
+
 sealed class FormState {
     object Idle : FormState ()
     object Loading : FormState ()
     data class Success (val message: String) : FormState ()
     data class Error (val message: String) : FormState ()
 }
+
+data class MahasiswaEvent (
+    val nim: String = "",
+    val nama: String = "",
+    val jenisKelamin: String = "",
+    val alamat: String = "",
+    val kelas: String = "",
+    val angkatan: String = ""
+)
